ccddf305f3e1053b0c4554f358b6c335ad0b1d36
Sakthi Kumar <sakthikumarmdu@gmail.com>
2015-06-24 17:47:54 +0530

    Add Dictate Type VC

---
+++ Dictastar/DictateTypeViewController.h
@@ -0,0 +1,13 @@
+//
+//  DictateTypeViewController.h
+//  Dictastar
+//
+//  Created by Bala Kumaran on 24/06/15.
+//  Copyright (c) 2015 mohamed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DictateTypeViewController : UIViewController
+@property (nonatomic, strong) NSDictionary *dataDict;
+@end
